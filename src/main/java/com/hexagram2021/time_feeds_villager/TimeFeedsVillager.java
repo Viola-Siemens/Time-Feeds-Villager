e391e52b60b8c69c37158defecd515d4979ef970
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2025-06-11 23:08:15 +0800

    Initial Commit

---
+++ src/main/java/com/hexagram2021/time_feeds_villager/TimeFeedsVillager.java
@@ -0,0 +1,11 @@
+package com.hexagram2021.time_feeds_villager;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(TimeFeedsVillager.MODID)
+public class TimeFeedsVillager {
+	public static final String MODID = "time_feeds_villager";
+
+	public TimeFeedsVillager() {
+	}
+}
